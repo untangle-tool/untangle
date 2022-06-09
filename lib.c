f1bddb026de3a914526a848ff17f690c33fd2720
Alessandro Bertani <alessandro.bertani@mail.polimi.it>
2022-06-09 15:12:05 +0000

    Updated function signatures for a different test case.
    Signed-off-by: Alessandro Bertani <alessandro.bertani@mail.polimi.it>

--- lib.c
+++ lib.c
@@ -4,26 +4,30 @@
 static unsigned flag = 0xfff;
 void (*func_ptr)(void) = NULL;
 
-static void static_func1(int a) {
-	if (a == 0x423452)
+static void target_function(void){
+	return;
+}
+
+static void static_func1(int a, char b) {
+	if (a == 0x423452 && b == 'a')
 		target_function();
 }
 
-void exported_func1(int a) {
-	static_func1(a);
+void exported_func1(int a, char b) {
+	static_func1(a, b);
 }
 
 void exported_func2(void) {
-	static_func1(2345);
+	static_func1(2345, 0);
 }
 
 static void static_func2(void) {
-	static_func1(654);
+	static_func1(654, 0);
 }
 
 static void static_func3(void) {
 	if (flag & 0xf000f)
-		static_func1(2345);
+		static_func1(2345, 0);
 }
 
 void exported_func3(void) {
@@ -34,6 +38,3 @@ void exported_func4(void) {
 	exported_func2();
 }
 
-static void target_function(void){
-	return;
-}
--- lib.h
+++ lib.h
@@ -1,4 +1,4 @@
-void exported_func1(int a);
+void exported_func1(int a, char b);
 void exported_func2(void);
 void exported_func3(void);
 void exported_func4(void);
