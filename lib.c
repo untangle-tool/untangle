246585ad3159cb80142fdce9b739132ff01d8c1a
Alessandro Bertani <alessandro.bertani@mail.polimi.it>
2022-06-09 10:21:25 +0200

    Added new test function and updated declaration in header file.
    Signed-off-by: Alessandro Bertani <alessandro.bertani@mail.polimi.it>

--- lib.c
+++ lib.c
@@ -4,26 +4,26 @@
 static unsigned flag = 0xfff;
 void (*func_ptr)(void) = NULL;
 
-static void static_func1(void) {
-	if (func_ptr)
-		func_ptr();
+static void static_func1(int a) {
+	if (a == 0x423452)
+		target_function();
 }
 
-void exported_func1(void) {
-	static_func1();
+void exported_func1(int a) {
+	static_func1(a);
 }
 
 void exported_func2(void) {
-	static_func1();
+	static_func1(2345);
 }
 
 static void static_func2(void) {
-	static_func1();
+	static_func1(654);
 }
 
 static void static_func3(void) {
 	if (flag & 0xf000f)
-		static_func1();
+		static_func1(2345);
 }
 
 void exported_func3(void) {
@@ -32,4 +32,8 @@ void exported_func3(void) {
 
 void exported_func4(void) {
 	exported_func2();
+}
+
+static void target_function(void){
+	return;
 }
