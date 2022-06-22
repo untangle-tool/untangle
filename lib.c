53f180d349bad12e7ef9fc1b8f0a26ad6b23dec8
Alessandro Bertani <alessandro.bertani.ab@gmail.com>
2022-06-22 22:57:21 +0200

    Modified lib.c to include a global variable as test case.
    Signed-off-by: Alessandro Bertani <alessandro.bertani.ab@gmail.com>

--- lib.c
+++ lib.c
@@ -1,7 +1,7 @@
 #include <stddef.h>
 #include "lib.h"
 
-static unsigned flag = 0xfff;
+static int flag1 = 0xfff;
 void (*func_ptr)(void) = NULL;
 
 static void target_function(void){
@@ -9,7 +9,7 @@ static void target_function(void){
 }
 
 static void static_func1(int a, char b) {
-	if (a == 0x423452 && b == 'a')
+	if (a == 0x423452 && (flag1 | 0xff000) == 0xfffff)
 		target_function();
 }
 
@@ -26,7 +26,7 @@ static void static_func2(void) {
 }
 
 static void static_func3(void) {
-	if (flag & 0xf000f)
+	if (flag1 & 0xf000f)
 		static_func1(2345, 0);
 }
 
