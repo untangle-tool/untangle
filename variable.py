e192a1c3061938d5831c746521b830c5e859d617
Alessandro Bertani <alessandro.bertani@mail.polimi.it>
2022-07-04 16:05:33 +0000

    Added a docstring to __init__, added address as argument and default values.
    Signed-off-by: Alessandro Bertani <alessandro.bertani@mail.polimi.it>

--- variable.py
+++ variable.py
@@ -1,5 +1,9 @@
 class Variable:
-    def __init__(self, name, type, size):
+    def __init__(self, name=None, type=None, size=0, address=0):
+        """ This class will represent a variable in the program. 
+            If type is None, then it is assumed to be a global variable.
+        """
+
         self.name = name
         self.type = type
         self.size = size
