56812cafb8020f0fa5c97b6a5b99ade3a5184cdc
Alessandro Bertani <alessandro.bertani@mail.polimi.it>
2022-07-07 15:08:10 +0000

    Modified test library.
    Signed-off-by: Alessandro Bertani <alessandro.bertani@mail.polimi.it>

--- libs/lib_globals.c
+++ libs/lib_globals.c
@@ -2,7 +2,7 @@
 #include "lib_globals.h"
 
 static int flag1;
-static int flag2[] = {0x1337, 0x1338, 0x1339};
+static int flag2 = 0x1337;
 
 void (*func_ptr)(void) = NULL;
 
@@ -11,7 +11,7 @@ static void target_function(void){
 }
 
 static void static_func1(int a, char b) {
-	if (a == 0x423452 && (flag1 | 0xff000) == 0xfffff && flag2[b] == 0xdeadbeef)
+	if (a == 0x423452 && (flag1 | 0xff000) == 0xfffff && flag2 == 0xdeadbeef)
 		target_function();
 }
 
@@ -21,10 +21,6 @@ void exported_func1(int a, char b) {
 
 void exported_func2(void) {
 	static_func1(2345, 0);
-}
-
-static void static_func2(void) {
-	static_func1(654, 0);
 }
 
 static void static_func3(void) {
