79e2f6da62b380e877edde0e14c7167c279bde34
Alessandro Bertani <alessandro.bertani@mail.polimi.it>
2022-07-15 10:37:46 +0000

    Added "concrete" and "value" properties to Variable.
    Signed-off-by: Alessandro Bertani <alessandro.bertani@mail.polimi.it>

--- variable.py
+++ variable.py
@@ -1,13 +1,15 @@
 class Variable:
-    def __init__(self, name=None, type=None, size=0, address=0):
+    def __init__(self, name=None, type=None, size=0, address=0, concrete=False, value=None):
         """ This class will represent a variable in the program. 
             If type is None, then it is assumed to be a global variable.
         """
-
+        
         self.name = name
         self.type = type
         self.size = size
         self.address = address
+        self.concrete = concrete
+        self.value = value
 
     def __repr__(self):
         return f"Name:{self.name}, type:{self.type}, size {self.size}, addr {hex(self.address)}"
